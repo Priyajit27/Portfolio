27e8737efbff3379d2f5e895397f90dd55b5a71c
Priyajit27 <priyajitpaul27@gmail.com>
2024-06-08 13:29:10 +0530

    Added few sections,needs modification

--- my-next-app/app/page.js
+++ my-next-app/app/page.js
@@ -1,18 +1,20 @@
 import Image from "next/image";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero"; 
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-    {/* <Navbar />
+    <Navbar />
     <div className="container mt-24 mx-auto px-12 py-4">
-      <HeroSection />
-      <AchievementsSection />
-      <AboutSection />
-      <ProjectsSection />
-      <EmailSection />
+      <Hero/>
+      <About />
+      {/* <ProjectsSection /> */}
+      {/* <EmailSection /> */}
     </div>
-    <Footer /> */}
-    <h1>HI</h1>
+    <Footer />
   </main>
   );
 }
