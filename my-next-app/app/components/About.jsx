49c48e2a827b7b1923aa80110b0c104bac3fd5f6
Priyajit27 <priyajitpaul27@gmail.com>
2024-07-20 10:47:45 +0530

    Babel issue solved

--- my-next-app/app/components/About.jsx
+++ my-next-app/app/components/About.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useTransition, useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import TabButton from "./TabButton";
 
 const TAB_DATA = [
